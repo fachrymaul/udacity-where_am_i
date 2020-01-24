d58cb937af6de725a83b22fb5024870702e328a6
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2020-01-24 08:42:57 +0700

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/fachry/Code/udacity/chase_it/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/fachry/Code/udacity/chase_it/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/fachry/Code/udacity/chase_it/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/fachry/Code/udacity/chase_it/src:$ROS_PACKAGE_PATH"
---
+++ build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fachry/Code/udacity/chase_it/src/my_robot
+# Build directory: /home/fachry/Code/udacity/chase_it/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
