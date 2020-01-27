56bcaee9e8b061832823e463a71aa5204c9b3bf7
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2020-01-27 21:46:39 +0700

    removu build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fachry/Code/udacity/chase_it/src
-# Build directory: /home/fachry/Code/udacity/chase_it/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_robot")
--- build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fachry/Code/udacity/chase_it/src/my_robot
-# Build directory: /home/fachry/Code/udacity/chase_it/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
