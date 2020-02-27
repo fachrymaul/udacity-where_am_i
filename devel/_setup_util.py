32f87e3f5ce5ed8c9cff7416479d9b36427171b1
Fachry Maulana Muhsinin <fachrymuhsinin@gmail.com>
2020-02-27 15:57:42 +0000

    feat: add map on myRobot

--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -270,7 +270,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = '/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = '/mnt/catkin-ws/udacity-where_am_i/devel;/opt/ros/kinetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
