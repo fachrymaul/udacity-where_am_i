036a09b866f6e67f48e4de29260f60b8bdd8d905
Fachry Maulana Muhsinin <fachrymuhsinin@outlook.co.id>
2020-02-28 16:39:25 +0700

    Build amcl launch (#1)
    * feat: add map on myRobot
    
    * feat: add amcl.launch to launch localization node

--- devel/local_setup.sh
+++ devel/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/fachry/Code/udacity/chase_it/devel}
+: ${_CATKIN_SETUP_DIR:=/mnt/catkin-ws/udacity-where_am_i/devel}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
